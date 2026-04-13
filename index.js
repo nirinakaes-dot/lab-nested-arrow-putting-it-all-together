5354768fb0e65a9387ab62cd0e4ba7fc9608cb4e
Nina Adora <nirinakaes@gmail.com>
2026-04-13 16:04:43 +0300

    Refactor login attempt logic in createLoginTracker function for clarity

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function createLoginTracker (x){
   return function innerFunction (passwordAttempt){
      attemptCount++ 
 console.log(attemptCount)
-    if(passwordAttempt === userInfo.password && attemptCount<=3) {return "Login succesful"} else if(attemptCount<=3){return `Attempt ${attemptCount}:Login failed`} else{ return "Account locked due to too many failed login attempts"}
+if(attemptCount >3) { return "Account locked due to too many failed login attempts"}
 
+    if(passwordAttempt === userInfo.password && attemptCount<=3) {return "Login succesful"} else if(attemptCount<=3){return `Attempt ${attemptCount}:Login failed`} 
   }
 
 }
