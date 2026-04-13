ab26431916eea1f942770865f45003975b580dc2
Nina Adora <nirinakaes@gmail.com>
2026-04-13 15:31:43 +0300

    Implement login tracker with attempt counting and account lockout feature

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+const userInfo = {
+    username : "user1",
+    password : "password123"
+}
 
+function createLoginTracker (x){
+    let attemptCount=0
 
+  return function innerFunction (attemptPassword){
+     attemptCount++ 
+console.log(attemptCount)
+    if(attemptPassword === userInfo.password && attemptCount<=3) {return "Login Succesfull"} else if(attemptCount<=3){return "Login failed"} else{ return "Account locked due to too many failed login attempts"}
+
+  }
+
+}
+const login = createLoginTracker(userInfo)
+console.log(login("password12345"))
+console.log(login("password1234"))
+console.log(login("password12347"))
+console.log(login("password1234"))
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
